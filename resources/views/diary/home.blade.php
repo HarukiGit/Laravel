edd21a84861f16c2bef2560b861bd4a90a567e16
メイãHarukiGit <tanakaharuki.jp@gmail.com>
2023-02-15 20:15:25 +0900

    ログイン機能を追加

--- resources/views/diary/home.blade.php
+++ resources/views/diary/home.blade.php
@@ -8,7 +8,21 @@
 </head>
 
 <body>
+    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+            {{ __('Log Out') }}
+        </x-responsive-nav-link>
+    </form>
+
+
     <h1>日記</h1>
+
+    @auth
     <div class="form">
         <form class="button" action="{{ route('write') }}" method="get">
             @csrf
@@ -19,6 +33,8 @@
             <button type="submit">削除</button>
         </form>
     </div>
+    @endauth
+
     @foreach ($diaries as $diary)
     <div class="diaries">
         <div class="date">{{ $diary['created_at'] }}</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,16 +18,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
 //テストページ
-Route::get('/t', [\App\Http\Controllers\test\IndexController::class,'show'])->name('test');
-Route::post('/t/post', [\App\Http\Controllers\test\IndexController::class,'button'])->name('button');
-Route::get('/pythonTest', [\App\Http\Controllers\python\python_test::class,'testRun']);
+Route::get('/t', [\App\Http\Controllers\test\IndexController::class, 'show'])->name('test');
+Route::post('/t/post', [\App\Http\Controllers\test\IndexController::class, 'button'])->name('button');
+Route::get('/pythonTest', [\App\Http\Controllers\python\python_test::class, 'testRun']);
 
 //日記サイト
 Route::get('/diary', \App\Http\Controllers\diary\home\IndexController::class)->name('home');
 
 Route::get('/diary/write', \App\Http\Controllers\diary\write\IndexController::class)->name('write');
-Route::post('/diary/write/create', \App\Http\Controllers\diary\write\CreateController::class)->name('create');
+Route::post('/diary/write/create', \App\Http\Controllers\diary\write\CreateController::class)->middleware('auth')->name('create');
 Route::post('/diary/write/create/image', \App\Http\Controllers\diary\write\UploadController::class)->name('create.image');
 
 Route::get('/diary/delete', \App\Http\Controllers\diary\delete\IndexController::class)->name('delete');
